55ee3602b4c265d3f3ab42e2feeea58c9f4e63ef
gereoffy <arpi.git@interoot.hu>
2023-11-04 17:53:07 +0000

    updated

--- word2vec/run.sh
+++ word2vec/run.sh
@@ -3,13 +3,13 @@
 if test -f all.txt; then
 
     # filter unwanted chars (emoji, chinese etc) from all.txt -> all.out
-    ./charfilter4.py
+    ./charfilter6.py
 
     # train SentencePiece tokenizer:
-    spm_train --input=all.out --vocab_size=262144 --max_sentencepiece_length=32 --model_prefix=spm5 --character_coverage=0.9999999 --max_sentence_length=65536 --remove_extra_whitespaces --train_extremely_large_corpus --hard_vocab_limit=true --num_threads=8 --shuffle_input_sentence=true
+    spm_train --model_prefix=spm6 --input=all.out6 --input_sentence_size=5000000 --vocab_size=262144 --max_sentencepiece_length=16 --character_coverage=1 --max_sentence_length=65536 --remove_extra_whitespaces --train_extremely_large_corpus --hard_vocab_limit=true --num_threads=8 --shuffle_input_sentence=false
 
     # tokenize input:
-    spm_encode --model=spm5.model --input=all.out --output=all.spm
+    spm_encode --model=spm6.model --input=all.out6 --output=all.spm --output_format=id
 
     # train gensim's Word2vec on tokens:
     ./wordvec1.py
