d38d7fecffdbd7fc05cc1e1e7de435703f269322
gereoffy <arpi.git@interoot.hu>
2023-11-07 22:07:28 +0000

    filename is optional

--- torch_eval.py
+++ torch_eval.py
@@ -17,13 +17,13 @@ def loadtokens(path):
 samples=[ loadtokens("SPAM-test.txt"), loadtokens("HAM-test.txt") ]
 
 
-for fnev in sys.argv[1:]:
+for fnev in sys.argv[1:] if len(sys.argv)>1 else ["model/deepspam.pt"]:
     ds.load(fnev)
     a=0;n=0
     ok=bad=0
     for text in open("Junk.txt","rt"):
         a+=(res:=ds(text.split("|",1))) ; n+=1
-        if len(sys.argv)==2: print("%6.3f%%"%res,text[:128])
+        if len(sys.argv)<=2: print("%6.3f%%"%res,text[:128])
         if res>80: ok+=1
         elif res<20: bad+=1
 #    if len(sys.argv)>2:  print("%d/%d  avg: %5.3f  [%s]"%(bad,ok,a/n,fnev)); continue
