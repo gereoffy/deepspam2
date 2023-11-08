ca64bc8183af07516c8cfa4a96b80c6fa15ec1a2
gereoffy <arpi.git@interoot.hu>
2023-11-08 16:31:15 +0000

    postpone samples tokenization

--- torch_eval.py
+++ torch_eval.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python3
 
-import time,os,sys
+import time,os,sys,glob
 from model import DeepSpam
 
 eval_bs=1024
+samples=None
 
 ds=DeepSpam(device="cuda",load=None,ds1=False)
 
@@ -14,20 +15,18 @@ def loadtokens(path):
         texts.append(t[:1024].split("|",1))
     return ds.tokenize(ds.preprocess(texts),ds.MAX_BLOCK)
 
-samples=[ loadtokens("SPAM-test.txt"), loadtokens("HAM-test.txt") ]
-
-
-for fnev in sys.argv[1:] if len(sys.argv)>1 else ["model/deepspam.pt"]:
+for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt'):
     ds.load(fnev)
     a=0;n=0
     ok=bad=0
     for text in open("Junk.txt","rt"):
         a+=(res:=ds(text.split("|",1))) ; n+=1
-        if len(sys.argv)<=2: print("%6.3f%%"%res,text[:128])
+        if len(sys.argv)==2: print("%6.3f%%"%res,text[:128])
         if res>80: ok+=1
         elif res<20: bad+=1
 #    if len(sys.argv)>2:  print("%d/%d  avg: %5.3f  [%s]"%(bad,ok,a/n,fnev)); continue
     # eval
+    if not samples: samples=[ loadtokens("SPAM-test.txt"), loadtokens("HAM-test.txt") ]
     t0=time.time()
     cnt={}
     for i in range(2):
