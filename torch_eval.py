30297ce80a56fd2916500f480d017758280db734
gereoffy <arpi.git@interoot.hu>
2023-11-19 08:45:40 +0000

    refactor eval/test to test() func

--- torch_eval.py
+++ torch_eval.py
@@ -6,14 +6,20 @@ from model import DeepSpam
 eval_bs=1024
 samples=None
 
-ds=DeepSpam(device="cuda",load=None,ds1=False)
+texts = []  # list of text samples
+labels = []  # list of label ids
+data = None
 
-def loadtokens(path):
-    texts=[]
+def loadtext(path,label_id):
     for t in open("data/"+path,"r"):
         if len(t)<10: continue # too short
         texts.append(t[:1024].split("|",1))
-    return ds.tokenize(ds.preprocess(texts),ds.MAX_BLOCK)
+        labels.append(label_id)
+
+loadtext("SPAM-test.txt",0)
+loadtext("HAM-test.txt",1)
+
+ds=DeepSpam(device="cuda",load=None,ds1=False)
 
 for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt'):
     ds.load(fnev)
@@ -26,21 +32,9 @@ for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt')
         elif res<20: bad+=1
 #    if len(sys.argv)>2:  print("%d/%d  avg: %5.3f  [%s]"%(bad,ok,a/n,fnev)); continue
     # eval
-    if not samples: samples=[ loadtokens("SPAM-test.txt"), loadtokens("HAM-test.txt") ]
+    if not data: data=ds.tokenize(ds.preprocess(texts),ds.MAX_BLOCK)
     t0=time.time()
-    cnt={}
-    for i in range(2):
-        cnt[i]=[0,0,0,0] # count, sum, hamtag, spamtag
-        x=0
-        while x<len(samples[i]):
-            for res in ds.evalbatch(samples[i][x:x+eval_bs],tokenized=True):
-                cnt[i][0]+=1
-                cnt[i][1]+=res
-                if res<20: cnt[i][2]+=1
-                if res>80: cnt[i][3]+=1
-            x+=eval_bs
+    val_loss,val_acc,test_acc,spam_stat=ds.test(data,labels)
     t=time.time()-t0
-    # results
-    print("%5.3f\t(%d/%d)\tham: %5.2f/%4.2f (%5.3f)  spam: %5.2f/%4.2f (%5.3f)  [%s]  %4.2fs (%d+%d)"%(a/n, bad,ok, 
-        100*cnt[1][2]/cnt[1][0],100*cnt[1][3]/cnt[1][0], cnt[1][1]/cnt[1][0],
-        100*cnt[0][3]/cnt[0][0],100*cnt[0][2]/cnt[0][0], cnt[0][1]/cnt[0][0],   fnev, t, cnt[0][0],cnt[1][0]))
+    print("%5.3f\t%5.3f\t(%d/%d)\t%s [%s]  %4.2fs"%(test_acc,a/n, bad,ok, spam_stat,  fnev, t))
+
