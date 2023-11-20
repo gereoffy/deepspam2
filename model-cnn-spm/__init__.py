b800bfdeb6787caac62474d13a13a0deb7d7ca15
gereoffy <arpi.git@interoot.hu>
2023-11-20 19:29:16 +0000

    refactor eval/test to test() func, use matplotlib to view weights

--- model-cnn-spm/__init__.py
+++ model-cnn-spm/__init__.py
@@ -11,16 +11,24 @@ class DeepSpam_model(torch.nn.Module):
     def __init__(self, ndim, filter_sizes=[2, 3, 4, 5], num_classes=2, dropout=0.5, filters=128, hidden=32):
         super().__init__()
         self.convl = torch.nn.ModuleList([ torch.nn.Conv1d(in_channels=ndim, out_channels=filters, kernel_size=k, padding="valid") for k in filter_sizes ])
-        self.l_dr2 = torch.nn.Dropout(dropout,inplace=True)
+        self.l_dr2 = torch.nn.Dropout(dropout,inplace=False)
         self.l_hid = torch.nn.Linear(len(filter_sizes)*filters, hidden)
+#        self.l_bn = torch.nn.BatchNorm1d(len(filter_sizes)*filters)
         self.l_fc = torch.nn.Linear(hidden, num_classes)
 
     def forward(self, x):
         x=x.permute(0, 2, 1) # reorder embedding->conv1d
-        x_conv = [ torch.max( torch.nn.functional.relu( conv(x) ), dim=2).values for conv in self.convl ] # Conv1D + ReLU + GlobalMaxPooling
+#        x_conv = [ torch.max( torch.nn.functional.relu( conv(x) ), dim=2).values for conv in self.convl ] # Conv1D + ReLU + GlobalMaxPooling
+        x_conv = [ torch.max(conv(x), dim=2).values for conv in self.convl ] # Conv1D + ReLU + GlobalMaxPooling
         x=torch.cat(x_conv,dim=1)
-        x = self.l_dr2(x) # drop 0.5
+#        x = self.l_bn(x)
+        x=torch.nn.functional.relu(x,inplace=True)
+
+#        x = self.l_dr2(x) # drop 0.5
         x = torch.nn.functional.relu(self.l_hid(x))  # linear 512->64 + ReLU
+#        x = torch.nn.functional.leaky_relu(self.l_hid(x))
+#        x = self.l_hid(x)
+#        x = torch.nn.functional.tanh(x)
         x = self.l_fc(x)   # linear 64->2
         return x
 
@@ -137,19 +145,84 @@ class DeepSpam:
     self.model=DeepSpam_model(self.num_dim)
     self.model.to(self.device)
 
+
+  def test(self,texts,label_ids,loss_fn=torch.nn.BCEWithLogitsLoss()):
+
+        data=torch.tensor(texts,dtype=torch.int,device=self.device)
+        labels=torch.nn.functional.one_hot(torch.tensor(label_ids),2).float().to(self.device)
+
+        # eval
+        test_spamcnt=test_hamcnt=0   # total no. of spam/ham in dataset
+        test_spam=test_ham=0         # number of correctly detected spam/ham
+        test_fp=test_fn=0            # number of incorrectly detected spam/ham
+        test_spamsum=test_hamsum=0.0 # sum of sigmoid results for spam/ham samples
+
+        self.model.eval()
+        with torch.no_grad():
+            val_acc=0.0
+            val_loss=0.0
+            i=0
+            while i<len(data):
+                eval_batch_size=1024 # batch_size*8
+                if i+eval_batch_size>len(data): eval_batch_size=len(data)-i
+                x=data[i:i+eval_batch_size]
+                y=labels[i:i+eval_batch_size]
+
+                logits=self.model(self.embedding(x))
+                val_loss+=loss_fn(logits,y).item()*eval_batch_size
+#                probs=logits.softmax(dim=1)  # FIXME: use sigmoid!
+                probs=logits.sigmoid()
+
+                _, acc_pred = probs.max(dim=1)
+                _, acc_good = y.max(dim=1)
+                acc=(acc_good == acc_pred).sum()
+                val_acc+=acc.item()
+
+                for res in probs:
+                    res0=res[0]/(res[0]+res[1]) # normalize sigmoid result
+                    if label_ids[i]==0:     ### SPAM sample  [0=negative]
+                        test_spamcnt+=1
+                        test_spamsum+=res0
+                        if res0>0.8: test_spam+=1
+                        elif res0<0.2: test_fn+=1  # number of spam detected as ham
+                    else: # label_ids[0]==1 ### HAM sample [1=positive]
+                        test_hamcnt+=1
+                        test_hamsum+=res0
+                        if res0<0.2: test_ham+=1
+                        elif res0>0.8: test_fp+=1  # number of ham detected as spam
+                    i+=1
+
+            val_acc/=i
+            val_loss/=i
+
+        # custom metrics for spam filtering:
+        test_acc=(1.0-test_spam/test_spamcnt) + 3.0*test_fn/test_spamcnt + 3.0*test_fp/test_hamcnt # ratio of non-detected spam + 3x ratio of FNs + 3x ratio of FPs
+
+        return val_loss,val_acc,test_acc, "HAM:%6.2f/%5.3f%% (%4.1f)  SPAM:%6.2f/%5.3f%% (%4.1f) "%(
+            100.0*test_ham/test_hamcnt, 100.0*test_fp/test_hamcnt, 100.0*test_hamsum/test_hamcnt,
+            100.0*test_spam/test_spamcnt, 100.0*test_fn/test_spamcnt, 100.0*test_spamsum/test_spamcnt )
+
+
   def train(self,texts,label_ids,num_train,epochs=15,batch_size=1024,max_len=MAX_BLOCK,dropwords=10,savebest=True,lr1=0.0001):
 
     self.log("HPARAMS: epochs=%d batch=%d blocklen=%d dropwords=%d"%(epochs,batch_size,max_len,dropwords))
 #    self.log("DATASET: %d train + %d eval = %d total   len: min=%d max=%d"%(num_train,len(texts)-num_train,len(texts), min(len(s) for s in texts), max(len(s) for s in texts) ))
     self.log("DATASET: %d train + %d eval = %d total"%(num_train,len(texts)-num_train,len(texts) ))
 
+    import matplotlib.pyplot as plt
+    plt.ion()
+    plt.clf() # clear fig
+#    plt.cla() # clear axis
+#    fig = plt.figure()
+#    plt2 = fig.add_subplot(111)
+
 #    print(self.tokenized(texts[:2]))
 
     # prepare dataset (array of texts and label_ids -> tokenized/onehot tensors):
     # data=self.tokenize(self.preprocess(texts),max_len)
     
-    data=torch.tensor(texts,dtype=torch.int,device=self.device)
-    labels=torch.nn.functional.one_hot(torch.tensor(label_ids),2).float().to(self.device)
+    data=torch.tensor(texts[:num_train],dtype=torch.int,device=self.device)
+    labels=torch.nn.functional.one_hot(torch.tensor(label_ids[:num_train]),2).float().to(self.device)
 #    print('Shape of data tensor:' + str(data.shape))    #Shape of data tensor: (118952, 100)
 #    print('Shape of label tensor:' + str(labels.shape)) #Shape of label tensor: (118952, 2)
 
@@ -158,7 +231,7 @@ class DeepSpam:
     for i in range(max_len):
         m=[1]*max_len
         m[i]=0
-#        m[(i+1)%max_len]=0 # mask token pair...
+        m[(i+1)%max_len]=0 # mask token pair...
         masks.append(m)
     masks=torch.tensor(masks).to(self.device)
 
@@ -169,7 +242,6 @@ class DeepSpam:
     #optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-07) # keras defaults
     optimizer = torch.optim.AdamW(self.model.parameters(), lr=lr1,  weight_decay=1e-1, betas=(0.9, 0.95) )
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs, 0.1*lr1, verbose=False) # 2x jobb a final loss/acc vele mint nelkule!
-#    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
 
     ep_size=num_train//batch_size
     val_loss=0
@@ -191,7 +263,6 @@ class DeepSpam:
         for step in range(ep_size):
 
             # get batch
-#            ix = torch.randint(num_train-batch_size, (batch_size,))
             ix = shufflei[step*batch_size:step*batch_size+batch_size]
             x = data[ix]
             y = labels[ix]
@@ -220,67 +291,19 @@ class DeepSpam:
         t_acc/=ep_size*batch_size
 
         t1=time.time()
+        val_loss,val_acc,test_acc,spam_stat=self.test(texts[num_train:],label_ids[num_train:],loss_fn)
+        t2=time.time()
 
-        # eval
-        test_spamcnt=test_hamcnt=0   # total no. of spam/ham in dataset
-        test_spam=test_ham=0         # number of correctly detected spam/ham
-        test_fp=test_fn=0            # number of incorrectly detected spam/ham
-        test_spamsum=test_hamsum=0.0 # sum of sigmoid results for spam/ham samples
+# ---------- PLOT -------------
+#        plot_y.append(t_loss)
+#        plt2.plot(plot_x,plot_y)
+#        plt.show()
+#        plt.draw()
+#        plt.pause(1)
+#        fig.canvas.draw()
+#        fig.canvas.flush_events()
 
-        self.model.eval()
-        with torch.no_grad():
-            val_acc=0.0
-            val_loss=0.0
-            i=num_train
-            while i<len(data):
-                eval_batch_size=batch_size*8
-                if i+eval_batch_size>len(data): eval_batch_size=len(data)-i
-                x=data[i:i+eval_batch_size]
-                y=labels[i:i+eval_batch_size]
-
-                logits=self.model(self.embedding(x))
-                val_loss+=loss_fn(logits,y).item()*eval_batch_size
-#                probs=logits.softmax(dim=1)  # FIXME: use sigmoid!
-                probs=logits.sigmoid()
-
-                _, acc_pred = probs.max(dim=1)
-                _, acc_good = y.max(dim=1)
-                acc=(acc_good == acc_pred).sum()
-                val_acc+=acc.item()
-
-                for res in probs:
-                    res0=res[0]/(res[0]+res[1]) # normalize sigmoid result
-                    if label_ids[i]==0:     ### SPAM sample  [0=negative]
-                        test_spamcnt+=1
-                        test_spamsum+=res0
-                        if res0>0.8: test_spam+=1
-                        elif res0<0.2: test_fn+=1  # number of spam detected as ham
-                    else: # label_ids[0]==1 ### HAM sample [1=positive]
-                        test_hamcnt+=1
-                        test_hamsum+=res0
-                        if res0<0.2: test_ham+=1
-                        elif res0>0.8: test_fp+=1  # number of ham detected as spam
-
-#                    res1=res[1]/(res[0]+res[1])
-#                    if label_ids[i]==0: test_spamcnt+=1 # 0=negative (spam)
-#                    else: test_hamcnt+=1                # 1=positive (ham)
-#                    if res1>0.8: # ham detected
-#                        if label_ids[i]==1: test_ham+=1
-#                        else: test_fn+=1  # number of spam detected as ham
-#                    elif res0>0.8: # spam detected
-#                        if label_ids[i]==0: test_spam+=1
-#                        else: test_fp+=1  # number of ham detected as spam
-
-                    i+=1
-
-            i-=num_train
-            val_acc/=i
-            val_loss/=i
-
-        # custom metrics for spam filtering:
-        test_acc=(1.0-test_spam/test_spamcnt) + 3.0*test_fn/test_spamcnt + 3.0*test_fp/test_hamcnt # ratio of non-detected spam + 3x ratio of FNs + 3x ratio of FPs
-
-        if ep<3: #epochs/5:
+        if ep<1: #epochs/5:
             is_best='.' # warmup :)
             best_acc=test_acc
         elif test_acc<best_acc-0.0001:
@@ -291,10 +314,31 @@ class DeepSpam:
             if savebest: self.save(saved)
         else: is_best=' '
 
-        self.log("%3d:  loss=%6.4f acc=%6.4f  val: %6.4f / %6.4f / %6.4f %s (%5.2f+%4.2f sec) lr:%10.8f  HAM:%6.2f/%5.3f%% (%4.1f)  SPAM:%6.2f/%5.3f%% (%4.1f) "%
-            (ep+1, t_loss,t_acc, val_loss,val_acc,test_acc, is_best, t1-t0, time.time()-t1, t_lr,
-            100.0*test_ham/test_hamcnt, 100.0*test_fp/test_hamcnt, 100.0*test_hamsum/test_hamcnt,
-            100.0*test_spam/test_spamcnt, 100.0*test_fn/test_spamcnt, 100.0*test_spamsum/test_spamcnt ) )
+
+        ww=self.model.l_hid.weight.view(-1)
+        ws=ww.std().item()
+        w=ww.tolist()
+#        wneg=[x for x in w if x<=0]
+#        wpos=[x for x in w if x>=0]
+#        wneg=sum(wneg)/len(wneg)
+#        wpos=sum(wpos)/len(wpos)
+#        wmid=[x for x in w if wneg/5<x<wpos/5]
+        wmid=[x for x in w if abs(x)<ws*0.1]
+
+        self.log("%3d:  loss=%6.4f acc=%6.4f  val: %6.4f / %6.4f / %6.4f %s (%5.2f+%4.2f sec) lr:%10.8f  %s  W:%5.3f|%5.3f"%
+            (ep+1, t_loss,t_acc, val_loss,val_acc,test_acc,  is_best, t1-t0, t2-t1, t_lr, spam_stat, ws,len(wmid)/len(w) ) )
+
+#        print(self.model.l_hid.weight.shape,self.model.l_fc.weight.shape) # torch.Size([32, 512]) torch.Size([2, 32])
+#        ww=self.model.l_fc.weight.view(-1).tolist()
+        plt.hist(w,256)
+        plt.show()
+        plt.pause(1)
+
+        if len(wmid)/len(w)>0.25: break # more than 25% of weights near zero...
+
+#    plt.plot(plot_x,plot_y,label=str(lr1))
+#    plt.show()
+#    plt.pause(1)
 
     return saved
 
