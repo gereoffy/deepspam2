541b381ecbf4cc68d7235b405ca42261ae5dbd29
gereoffy <arpi.git@interoot.hu>
2023-11-21 10:09:08 +0000

    ascii-art bitmap of weights

--- torch_eval.py
+++ torch_eval.py
@@ -10,6 +10,15 @@ texts = []  # list of text samples
 labels = []  # list of label ids
 data = None
 
+def draw_bitmap(w):
+    chrmap=" ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█"    # 0=.. 1=*. 2=.* 3=**
+    print(len(w),len(w[0]),w[0][0]) # 32 512 0.756577730178833
+    def get(x,y): return 0 if w[y][x]<0.3 else 1
+    for y in range(0,len(w),2):
+        s=""
+        for x in range(0,min(len(w[0]),256),2): s+=chrmap[ get(x,y) + get(x+1,y)*2 + get(x,y+1)*4 + get(x+1,y+1)*8 ]
+        print(s,file=sys.stderr)
+
 def draw_hist(w,wmin,wmax,col=128,row=8):
     ww=wmax-wmin
     data=[0]*col
@@ -38,7 +47,13 @@ loadtext("HAM-test.txt",1)
 ds=DeepSpam(device="cuda",load=None,ds1=False)
 
 for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt'):
-    ds.load(fnev)
+    try:
+        ds.load(fnev)
+    except Exception as e:
+        print("FAILED:",fnev,repr(e))
+        continue
+
+    ds.plot(clear=True)
 
     ww=ds.model.l_hid.weight.view(-1)
 #    ww=ds.model.l_fc.weight.view(-1)
@@ -46,7 +61,10 @@ for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt')
     w=ww.tolist()
     wmid=[x for x in w if abs(x)<ws*0.1]
     draw_hist(w,-5*ws,5*ws)
-    ds.plot(clear=True)
+
+    ww=ds.model.l_hid.weight * ds.model.l_fc.weight[0].unsqueeze(dim=1)
+    ww=ww.abs()/ww.std()
+    draw_bitmap(ww.tolist())
 
     a=0;n=0
     ok=bad=0
@@ -57,6 +75,7 @@ for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt')
         if res>80: ok+=1
         elif res<20: bad+=1
 #    if len(sys.argv)>2:  print("%d/%d  avg: %5.3f  [%s]"%(bad,ok,a/n,fnev)); continue
+
     # eval
     if not data: data=ds.tokenize(ds.preprocess(texts),ds.MAX_BLOCK)
     t0=time.time()
@@ -64,3 +83,5 @@ for fnev in sys.argv[1:] if len(sys.argv)>1 else glob.glob('model/deepspam*.pt')
     t=time.time()-t0
     print("%5.3f\t%5.3f\t(%d/%d)\t%s [%s]  %4.2fs    %5.3f|%5.3f"%(test_acc,a/n, bad,ok, spam_stat,  fnev, t, ws,len(wmid)/len(w) ))
 
+#    time.sleep(10)
+    
